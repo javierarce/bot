65e45bcc66d8f079d5999524164f430ceaf4a8c4
Javier Arce <javierarce@gmail.com>
2015-09-19 18:01:49 +0200

    Improves test and example

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,9 +4,19 @@ var Bot = require('../lib/bot.js');
 var assert = require('assert');
 
 suite('Bot', function() {
-  test('processTweet should return the number of characters', function(done) {
-    var tweet = { text: "hello" };
-    assert.equal(Bot._processTweet(tweet), 5);
+  test('processTweet should return the feeling', function(done) {
+    var tweet = { text: 'I am feeling happy' };
+    assert.equal(Bot._processTweet(tweet), 'happy');
+
+    tweet = { text: 'I\'m feeling bored' };
+    assert.equal(Bot._processTweet(tweet), 'bored');
+
+    tweet = { text: 'I am bored' };
+    assert.equal(Bot._processTweet(tweet), null);
+
+    tweet = { text: 'Am I feeling ok?' };
+    assert.equal(Bot._processTweet(tweet), null);
+
     done();
   });
 });
